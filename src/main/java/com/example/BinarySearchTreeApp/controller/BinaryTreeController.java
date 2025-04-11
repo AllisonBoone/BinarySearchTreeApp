0a4bde493c7e9a7243d7582545ee02f5d65a9e7b
Allison <Allison.Boone@keyin.com>
2025-04-11 17:59:51 -0230

    Added html page and edited tree controller to load html page correctly

--- src/main/java/com/example/BinarySearchTreeApp/controller/BinaryTreeController.java
+++ src/main/java/com/example/BinarySearchTreeApp/controller/BinaryTreeController.java
@@ -5,21 +5,28 @@ import com.example.BinarySearchTreeApp.model.TreeRecord;
 import com.example.BinarySearchTreeApp.service.BinaryTreeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
 
-@RestController
-@RequestMapping("/process-numbers")
+@Controller
 public class BinaryTreeController {
 
     @Autowired
     private BinaryTreeService binaryTreeService;
 
-    // Error handling. 
-    @PostMapping
+    // Store numbers with error handling. 
+    @PostMapping("/process-numbers")
+    @ResponseBody
     public TreeRecord processNumbers(@RequestParam String input) {
         try {
             return binaryTreeService.buildAndSaveTree(input);
         } catch (Exception e) {
             throw new RuntimeException("Failed to process input: " + e.getMessage());
         }
+    }
+
+    // Loads HTML form.
+    @GetMapping("/enter-numbers")
+    public String enterNumbersPage() {
+        return "enter-numbers";
     }
 }
