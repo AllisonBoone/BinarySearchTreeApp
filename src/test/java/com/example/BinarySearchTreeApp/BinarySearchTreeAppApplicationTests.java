064b40f92ce00b60b8e1c0be334f9b6d20c7d91e
Allison <Allison.Boone@keyin.com>
2025-04-11 16:16:00 -0230

    first commit

---
+++ src/test/java/com/example/BinarySearchTreeApp/BinarySearchTreeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BinarySearchTreeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinarySearchTreeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
